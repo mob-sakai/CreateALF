13b4bc386c9b8dd19ec15456a8678b3dead20749
mob-sakai <sakai@mobcast.jp>
2019-12-04 18:12:34 +0900

    ...

--- activate.js
+++ activate.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 ;(async () => {
-  const browser = await puppeteer.launch({args: ["--no-sandbox", "--disable-setuid-sandbox"]})
+  const browser = await puppeteer.launch({ executablePath: 'google-chrome-unstable' })
   const page = await browser.newPage()
 
   const downloadPath = process.cwd()
